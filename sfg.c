d4872c0c59214468f70e7bad78b07ae200ab3fac
MarcelFerrari <37771642+MarcelFerrari@users.noreply.github.com>
2018-12-31 15:46:18 +0100

    Update sfg.c

--- sfg.c
+++ sfg.c
@@ -1,3 +1,19 @@
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    Copyright © 2018 Marcel Ferrari
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -22,7 +38,7 @@ int main (int argc, char *argv[])
   // Too few or many arguments
   if (argc != 2 && (argc < 4 || argc > 6))
     {
-      fprintf (stderr, "Usage: sfg [path] [size] [unit] [bs] [filenamename]\n");
+      fprintf (stderr, "Usage: sfg [path] [size] [unit] [bs] [filename]\n");
       return 1;
     }
   else if (argc == 2 && strcmp (argv[1], "--help") == 0)
